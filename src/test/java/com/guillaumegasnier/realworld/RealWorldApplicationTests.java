e63b17b0e56808830122e2dd9b4e0dd7f36f1e76
ggasnier <23357977+ggasnier@users.noreply.github.com>
2024-12-29 10:59:26 +0100

    Initial commit

---
+++ src/test/java/com/guillaumegasnier/realworld/RealWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guillaumegasnier.realworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
